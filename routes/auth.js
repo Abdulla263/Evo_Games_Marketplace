025b99f0461600e7e69ed9085449789c6198bf29
ammarys-w <ammar39876@gmail.com>
2025-11-12 16:30:32 +0300

    i search in the AI and it is not working about the upload profile pic

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const router = require("express").Router()
+
+
+//auth controller
+const authCtrl = require("../controllers/auth")
+const upload = require("../middleware/multer")
+//router / calling API's
+
+router.get("/sign-up", authCtrl.auth_signup_get)
+router.post("/sign-up", upload.single("pfp"), authCtrl.auth_signup_post)
+
+router.get("/sign-in", authCtrl.auth_signin_get)
+router.post("/sign-in", authCtrl.auth_signin_post)
+
+
+module.exports = router
