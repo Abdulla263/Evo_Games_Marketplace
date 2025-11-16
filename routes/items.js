0ea86b2e589aa70ca46b42c1462a625717b1325f
mohdrahk <mohammedrak@gmail.om>
2025-11-16 19:05:34 +0300

    Items creation router and controllers added

--- routes/items.js
+++ routes/items.js
@@ -8,4 +8,9 @@ router.get("/", itemsCtrl.listing_index_get)
 router.get("/new", itemsCtrl.item_create_get)
 router.post("/", itemsCtrl.items_create_post)
 
+// router.get("/:listingId", itemsCtrl.listing_show_get)
+// router.get("/:listingId/edit", itemsCtrl.listing_edit_get)
+// router.put("/:listingId", itemsCtrl.listing_edit_put)
+// router.delete("/:listingId", itemsCtrl.listing_remove_delete)
+
 module.exports = router
