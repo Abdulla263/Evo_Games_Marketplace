30936346840d8c638785c33bcdb9581ff7b12559
mohdrahk <mohammedrak@gmail.om>
2025-11-16 18:57:39 +0300

    Items creation router and controllers added

--- controllers/items.js
+++ controllers/items.js
@@ -5,3 +5,18 @@ exports.items_index_get = async (req, res) => {
   const items = await Item.find().populate("owner")
   res.render("listings/index.ejs", { items })
 }
+
+//Show create form
+exports.item_create_get = async (req, res) => {
+  res.render("items/new.ejs")
+}
+
+// Create item
+exports.items_create_post = async (req, res) => {
+  req.body.owner = req.session.user._id
+  if (req.file) req.body.image = req.file.filename
+  await Item.create(req.body)
+  res.redirect("/items")
+}
+
+
--- routes/items.js
+++ routes/items.js
@@ -3,3 +3,9 @@ const itemsCtrl = require("../controllers/items")
 const upload = require("../middleware/multer")
 
 router.get("/", itemsCtrl.items_index_get)
+
+router.get("/", itemsCtrl.listing_index_get)
+router.get("/new", itemsCtrl.item_create_get)
+router.post("/", itemsCtrl.items_create_post)
+
+module.exports = router
