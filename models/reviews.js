09a2de0303a10c3512e82950d3e596024525a896
ammarys-w <ammar39876@gmail.com>
2025-11-18 18:30:14 +0300

    pushing the model

--- models/reviews.js
+++ models/reviews.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
-const itemSchema = new mongoose.Schema({
-  item_id: {
-  type:Number,
-  required: true
-  },
-  user_id: {
-    type: Number,
+
+const ReviewSchema = new mongoose.Schema({
+  item: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Item',
     required: true
   },
-  pricereview: {
-    type:String,
-    required:true
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   },
-  createTime:{
-    type:Date
+  voteType: {
+    type: String,
+    enum: ['fair', 'high'],
+    required: true
   }
-})
+}, {
+  timestamps: true // make timestamp outsdie the schema, MOngoDB will handle the updates , crates at at it as automatic
+  //https://www.geeksforgeeks.org/mongodb/mongoose-timestamps/
+});
 
+priceReviewSchema.index({ item: 1, user: 1 }, { unique: true });
+// this to make the users make ONLY one review to specific ONE item => to avoid duplicate reviews
+
+const ReviewModel = mongoose.model('ReviewModel', ReviewSchema);
+module.exports = ReviewModel
