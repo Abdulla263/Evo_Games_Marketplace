26d0edf8152fca89082101623780d47a18559352
mohdrahk <mohammedrak@gmail.om>
2025-11-19 14:32:35 +0300

    user can vote, database unknown issue fixed

--- routes/items.js
+++ routes/items.js
@@ -3,9 +3,13 @@ const itemsCtrl = require("../controllers/items")
 const upload = require("../middleware/multer")
 const isSignedIn = require("../middleware/is-signed-in")
 
+const reviewCtrl = require("../controllers/reviews")
+
 router.get("/", itemsCtrl.items_index_get)
 router.get("/category/:category", itemsCtrl.items_category_get)
 router.get("/new", isSignedIn, itemsCtrl.item_create_get)
+router.post("/:itemId/price-review", isSignedIn, reviewCtrl.submitReview)
+router.delete("/:itemId/price-review", isSignedIn, reviewCtrl.deleteReview)
 router.get("/:itemId", itemsCtrl.items_show_get)
 
 router.post(
